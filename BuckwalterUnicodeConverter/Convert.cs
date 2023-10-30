405ec156e7b91bd5e9b64ee7660fd20085858b34
SuhaibUmerBhatti <doctor_suhaib@hotmail.com>
2023-10-30 21:07:48 +0000

    Add project files.

---
+++ BuckwalterUnicodeConverter/Convert.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+
+namespace ArabicUnicodeBuckwalterConverter
+{
+    internal class Convert
+    {
+        private static char[] uCodes = {'\u0628', '\u0650', '\u0633', '\u0652', '\u0645', '\u0671', '\u0644', '\u0651', '\u064E', '\u0647', '\u0631', '\u062D', '\u0670', '\u0646', '\u064A', '\u062F', '\u064F', '\u0639', '\u0643', '\u0648', '\u0625', '\u0627', '\u062A', '\u0635', '\u0637', '\u0642', '\u0630', '\u0623', '\u063A', '\u0636', '\u0653', '\u0641', '\u064B', '\u0649', '\u0624', '\u0629', '\u0632', '\u0621', '\u062E', '\u06DF', '\u0626', '\u064C', '\u0634', '\u0638', '\u06E2', '\u062C', '\u062B', '\u06E5', '\u064D', '\u06E6', '\u0640', '\u0654', '\u06ED', '\u06DC', '\u06E0', '\u06EA', '\u06EB', '\u06E8', '\u06EC', '\u06E3', '\u0020'};
+        private static char[] bCodes = {'b', 'i', 's', 'o', 'm', '{', 'l', '~', 'a', 'h', 'r', 'H', '`', 'n', 'y', 'd', 'u', 'E', 'k', 'w', '<', 'A', 't', 'S', 'T', 'q', '*', '>', 'g', 'D', '^', 'f', 'F', 'Y', '&', 'p', 'z', '\'', 'x', '@', '}', 'N', '$', 'Z', '[', 'j', 'v', ',', 'K', '.', '_', '#', ']', ':', '"', '-', '+', '!', '%', ';',' '};
+        
+        /// <summary>
+        /// To check if passed string is Buckwalter transliteration.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private static bool isBuckwalter(string str)
+        {
+            bool result = true;
+            try
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (((short)str[i]) > 500)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Error occured in 'isBuckwalter' function");
+                return false;
+            }
+        }
+        
+        /// <summary>
+        /// Converts Arabic unicode string to Buckwalter transliteration.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToBuckwalter(string str)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < str.Length; i++)
+                    sb.Append(bCodes[Array.IndexOf(uCodes, str[i])]);
+                return sb.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"Error occured in 'ToBuckwalter' function. String not converted.");
+                return str;
+            }
+        }
+        
+        /// <summary>
+        /// Coverts Buckwalter string to Arabic unicode string.
+        /// </summary>
+        /// <returns></returns>
+        public static string ToArabicUnicode(string str)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < str.Length; i++)
+                    sb.Append(uCodes[Array.IndexOf(bCodes, str[i])]);
+                return sb.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"Error occured in 'ToArabicUnicode' function. String not converted.");
+                return str;
+            }
+        }
+
+        /// <summary>
+        /// Checks the string and converts accordingly.
+        /// From Buckwalter string to Arabic Unicode string.
+        /// and from Arabic Unicode string to Buckwalter string.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string This(string str)
+        {
+            if (isBuckwalter(str))
+                return ToArabicUnicode(str);
+            else
+                return ToBuckwalter(str);
+        }
+    }
+}
---
+++ BuckwalterUnicodeConverter/frmMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ArabicUnicodeBuckwalterConverter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnConvert = new Button();
+            txtText = new TextBox();
+            txtConversion = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            btnConvert.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnConvert.Location = new Point(915, 483);
+            btnConvert.Name = "btnConvert";
+            btnConvert.Size = new Size(247, 46);
+            btnConvert.TabIndex = 0;
+            btnConvert.Text = "Convert";
+            btnConvert.UseVisualStyleBackColor = true;
+            btnConvert.Click += btnConvert_Click;
+            // 
+            // txtText
+            // 
+            txtText.Location = new Point(12, 44);
+            txtText.Multiline = true;
+            txtText.Name = "txtText";
+            txtText.Size = new Size(1150, 188);
+            txtText.TabIndex = 1;
+            // 
+            // txtConversion
+            // 
+            txtConversion.Location = new Point(12, 280);
+            txtConversion.Multiline = true;
+            txtConversion.Name = "txtConversion";
+            txtConversion.Size = new Size(1150, 188);
+            txtConversion.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 32);
+            label1.TabIndex = 3;
+            label1.Text = "Text";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(12, 245);
+            label2.Name = "label2";
+            label2.Size = new Size(142, 32);
+            label2.TabIndex = 4;
+            label2.Text = "Convertion";
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1174, 546);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtConversion);
+            Controls.Add(txtText);
+            Controls.Add(btnConvert);
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Main";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnConvert;
+        private TextBox txtText;
+        private TextBox txtConversion;
+        private Label label1;
+        private Label label2;
+    }
+}
